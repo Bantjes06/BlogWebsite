e603715cf2b1895c1f29b07df1015dce525dbc0b
Johan <Bantjes06@gmail.com>
2023-11-16 17:29:02 +0200

    UI almost done, view and edit logic to be done

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+//Initializing express
+const app = express();
+
+//Declaring port
+const port = 3000;
+
+var entries = [];
+class Entry {
+    constructor(entryTitle, entryBody){
+    this.entryTitle = entryTitle,
+    this.entryBody = entryBody
+    };
+}
+
+//For CSS files
+app.use(express.static("public"));
+
+//To use req.body property
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Get request to home page
+app.get("/", (req, res) => {
+    res.render("index.ejs", {entries: entries});
+})
+
+app.get("/create", (req, res) => {
+    res.render("create_post.ejs");
+})
+
+app.post("/submit", (req, res) => {
+    var entryTitle = req.body["pTitle"];
+    var entryBody = req.body["pBody"];
+    const newEntry = new Entry(entryTitle, entryBody);
+    entries.push(newEntry);
+    console.log(entries);
+    res.redirect("/");
+    console.log(entries);
+})
+
+app.post('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    if (id >= 0 && id < entries.length) {
+        entries.splice(id, 1);
+    }
+    res.redirect('/');
+});
+
+
+console.log(entries);
+
+//Setting up Port 3000
+app.listen(port, () => {
+    console.log("Server up at Port 3000");
+})
